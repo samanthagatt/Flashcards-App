29c355cb79251e4908a48c7ff72bc0529bf9d777
Samantha Gatt <smfgatt@gmail.com>
2018-09-27 08:56:50 -0400

    Implements card flipping animation

--- Flashcards/Flashcards/View Controllers/CardDetailViewController.swift
+++ Flashcards/Flashcards/View Controllers/CardDetailViewController.swift
@@ -39,27 +39,47 @@ class CardDetailViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func save(_ sender: Any) {
-        guard let card = card else { return }
-        let text = textView.text ?? ""
-        if isFront {
-            cardController.update(card: card, front: text, context: CoreDataStack.moc)
-        } else if !isFront {
-            cardController.update(card: card, back: text, context: CoreDataStack.moc)
-        }
+        saveCard()
     }
     
     @IBAction func flip(_ sender: Any) {
         guard let card = card else { return }
         if textView.text != card.front && textView.text != card.back {
             let alert = UIAlertController(title: "Uh Oh!", message: "Looks like you haven't saved your changes", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            let saveAction = UIAlertAction(title: "Save", style: .default) { (_) in
+                self.saveCard()
+            }
+            alert.addAction(cancelAction)
+            alert.addAction(saveAction)
             self.present(alert, animated: true, completion: nil)
         } else if isFront {
-            textView.text = card.back
+            UIView.transition(with: cardView, duration: 1.0, options: [.transitionFlipFromRight], animations: {
+                self.textView.text = card.back
+            })
             isFront = false
         } else if textView.text == card.back {
-            textView.text = card.front
+            UIView.transition(with: cardView, duration: 1.0, options: [.transitionFlipFromRight], animations: {
+                self.textView.text = card.front
+            })
             isFront = true
+        }
+        
+        
+        
+    }
+    
+    
+    // MARK: - Functions
+    
+    func saveCard() {
+        guard let card = card else { return }
+        let text = textView.text ?? ""
+        
+        if isFront {
+            cardController.update(card: card, front: text, context: CoreDataStack.moc)
+        } else if !isFront {
+            cardController.update(card: card, back: text, context: CoreDataStack.moc)
         }
     }
 }
